66ad3dbf35943e43945edaa3aed267b0681f5bc6
Katrin <katabrovkova952@gmail.com>
2026-07-14 19:27:26 +0400

    changed with JWT

--- books/urls.py
+++ books/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from . import views
+from .views import home
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('book/<int:book_id>/', views.book_detail, name='book_detail'),
-    path('add/', views.add_book, name='add_book'),
-    path('book/<int:book_id>/edit/', views.edit_book, name='edit_book'),
-    path('book/<int:book_id>/delete/', views.delete_book, name='delete_book'),
+    path('', home, name='home'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
